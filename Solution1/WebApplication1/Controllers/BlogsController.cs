1d670d558a8c36c4d9d90fae27237c3baec0ea67
BuildTools <unconfigured@null.spigotmc.org>
2021-03-07 02:03:19 +0100

    Lesson 6 File Image Upload, Google Service Account

--- Solution1/WebApplication1/Controllers/BlogsController.cs
+++ Solution1/WebApplication1/Controllers/BlogsController.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Google.Cloud.Storage.V1;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using WebApplication1.Models.Domain;
 using WebApplication1.Services.Interfaces;
 
@@ -11,8 +14,10 @@ namespace WebApplication1.Controllers
     public class BlogsController : Controller
     {
         private readonly IBlogsRepository _blogsRepo;
-        public BlogsController(IBlogsRepository blogsRepo)
+        private readonly IConfiguration _config;
+        public BlogsController(IBlogsRepository blogsRepo, IConfiguration config)
         {
+            _config = config;
             _blogsRepo = blogsRepo;
         }
         public IActionResult Index()
@@ -28,13 +33,32 @@ namespace WebApplication1.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(Blog b)
+        public IActionResult Create(IFormFile logo, Blog b)
         {
             if (_blogsRepo.GetBlogs().Where(x => x.Url == b.Url).Count() == 0)
             {
                 _blogsRepo.AddBlog(b);
             }
             else TempData["warning"] = "Blog exists already";
+
+            string bucketName = _config.GetSection("AppSettings").GetSection("LogosBucket").Value;
+
+            if(logo != null)
+            {
+                var storage = StorageClient.Create();
+
+                string uniqueFilename = Guid.NewGuid() + System.IO.Path.GetExtension(logo.FileName);
+
+                using (var myfile = logo.OpenReadStream())
+                {
+                    storage.UploadObject(bucketName, uniqueFilename, null, myfile);
+                }
+
+                b.Url = $"https://storage.googleapis.com/{bucketName}/{uniqueFilename}";
+                _blogsRepo.AddBlog(b);
+            }
+
+
             return RedirectToAction("Index");
         }
     }
